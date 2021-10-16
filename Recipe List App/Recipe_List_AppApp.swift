b318a2685bb93e98eb2e3306c75d979ed6c77922
Tyler Cook <tycook1@gmail.com>
2021-10-16 12:17:27 -0600

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Tyler Cook on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
