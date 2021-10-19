1ff1263f7c8f704e4a469905a57857dd2596a18d
Tyler Cook <tycook1@gmail.com>
2021-10-19 11:12:36 -0600

    - Add RecipeFeaturedView - Modify data flow using environment object modifier - Create RecipeModel instance in the RecipeTabView

--- Recipe List App/Models/Recipe.swift
+++ Recipe List App/Models/Recipe.swift
@@ -18,7 +18,17 @@ class Recipe: Identifiable, Decodable {
     var cookTime: String
     var totalTime: String
     var servings: Int
-    var ingredients: [String]
+    var highlights: [String]
+    var ingredients: [Ingredient]
     var directions: [String]
     
+}
+
+class Ingredient: Identifiable, Decodable {
+    
+    var id: UUID?
+    var name: String
+    var numerator: Int?
+    var denominator: Int?
+    var unit: String?
 }
